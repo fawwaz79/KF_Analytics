5d2543da40e75b64ff344f0bb27fb1cec3f40e61
fawwaz79 <janissary519@gmail.com>
2026-09-14 12:08:33 +0700

    Enhance SQL query for transaction analysis
    Updated SQL query to include gross profit percentage and net sales calculations.

--- analisa_kf.sql
+++ analisa_kf.sql
@@ -1,18 +1,24 @@
 SELECT 
+    -- Ambil data transaksi
     t.transaction_id,
     t.date,
     t.branch_id,
+    
+    -- Ambil data cabang 
     c.branch_name,
     c.kota,
     c.provinsi,
     c.rating AS rating_cabang,
+    
+    -- Ambil data pelanggan dan detail barang yang dibeli
     t.customer_name,
     t.product_id,
     p.product_name,
     p.price AS actual_price,
     t.discount_percentage,
     
-
+    -- Menentukan persentase laba kotor
+    
     CASE
         WHEN p.price <= 50000 THEN 0.10
         WHEN p.price > 50000 AND p.price <= 100000 THEN 0.15
@@ -21,10 +27,8 @@ SELECT
         WHEN p.price > 500000 THEN 0.30
     END AS persentase_gross_laba,
     
-    
+    -- Hitung nett sales 
     p.price - (p.price * t.discount_percentage) AS nett_sales,
-    
-    
     (p.price - (p.price * t.discount_percentage)) * 
     CASE
         WHEN p.price <= 50000 THEN 0.10
@@ -33,14 +37,16 @@ SELECT
         WHEN p.price > 300000 AND p.price <= 500000 THEN 0.25
         WHEN p.price > 500000 THEN 0.30
     END AS nett_profit,
-    
+
     t.rating AS rating_transaksi
 
 FROM 
     `rakamin-kf-analytics-508503.kimia_farma.kf_final_transaction` t
+
 LEFT JOIN 
     `rakamin-kf-analytics-508503.kimia_farma.kf_kantor_cabang` c 
     ON t.branch_id = c.branch_id
+
 LEFT JOIN 
     `rakamin-kf-analytics-508503.kimia_farma.kf_product` p 
     ON t.product_id = p.product_id;
